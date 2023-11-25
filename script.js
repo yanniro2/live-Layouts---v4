b8fe235b94b60e342e48abfb174142ab247ba6a9
Niroyan Kugaruban <k.niroyan@gmail.com>
2023-11-25 09:24:47 +0530

    first name

---
+++ script.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+  theme: {
+    
+    extend: {},
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+      roboto: ["Roboto", "sans-serif"],
+      Digital: ["DS-Digital", "sans-serif"],
+      custom: ["DS-DIGIB", "sans-serif"],
+    },
+  },
+};
